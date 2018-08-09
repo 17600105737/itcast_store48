b9da1b8d827180e7cf1a1bb82d9b3ce51d282904
chen <17600105737@163.com>
2018-08-10 00:02:14 +0800

    用户列表展示

--- src/views/users/Users.vue
+++ src/views/users/Users.vue
@@ -13,12 +13,69 @@
         <el-button type="success" plain>成功按钮</el-button>
       </el-col>
     </el-row>
+    <el-table
+      stripe
+      border
+      :data="data"
+      style="width: 100%">
+      <el-table-column
+      type="index"
+      width="50">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="用户状态"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="操作">
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+  data(){
+    return {
+      data:[]
+    }
+  },
+  created(){
+    this.loadData()
+  },
+  methods:{
+    async loadData(){
+      // 发送请求的时候，要在请求头中添加Authorization=token
+      var token = sessionStorage.getItem('token');
+      axios.defaults.headers.common['Authorization'] = token;
 
+      var response = await axios.get
+      ('http://localhost:8888/api/private/v1/users?pagenum=1&pagesize=10');
+      var { meta:{status,msg}} = response.data;
+      if (status === 200) {
+        this.data = response.data.data.users;
+      } else {
+        this.$message.error(msg);
+      }
+    }
+  }
 }
 </script>
 
