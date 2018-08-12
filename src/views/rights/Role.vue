0a8c4ed5ad38d8a39cc33222752bdde13c460bda
chen <17600105737@163.com>
2018-08-12 20:11:47 +0800

    权限展示

--- src/views/rights/Role.vue
+++ src/views/rights/Role.vue
@@ -10,10 +10,30 @@
       stripe
       style="width: 100%">
       <el-table-column type="expand">
-      <template slot-scope="props">
-        <el-form label-position="left" inline class="demo-table-expand">
-          
-        </el-form>
+      <template slot-scope="scope">
+        <el-row class="level1Row" v-for="level1 in scope.row.children" :key="level1.id">
+          <el-col :span="4">
+            <!-- 一级权限 -->
+            <el-tag closable>
+              {{ level1.authName }}
+            </el-tag>
+          </el-col>
+          <el-col :span="20">
+            <el-row v-for="level2 in level1.children" :key="level2.id">
+              <el-col :span="4">
+                <!-- 二级权限 -->
+                <el-tag closable type="success">
+                  {{ level2.authName }}
+                </el-tag>
+              </el-col>
+              <el-col :span="20">
+                <el-tag class="level3Tag" closable type="warning" v-for="level3 in level2.children" :key="level3.id">
+                  {{ level3.authName }}
+                </el-tag>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
       </template>
     </el-table-column>
       <el-table-column
@@ -69,5 +89,12 @@ export default {
 .addButton {
   margin-top: 10px;
   margin-bottom: 5px;
+}
+.level3Tag {
+  margin-right: 5px;
+  margin-bottom: 5px;
+}
+.level1Row {
+  margin-bottom: 10px;
 }
 </style>
