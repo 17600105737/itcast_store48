de328827b1c787e9e37cc25f93e35105833fb507
chen <17600105737@163.com>
2018-08-15 20:38:02 +0800

    商品属性(静态参数)

--- src/views/goods/GoodsAdd.vue
+++ src/views/goods/GoodsAdd.vue
@@ -74,7 +74,15 @@
           </el-form-item>
         </el-tab-pane>
 
-        <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="2" label="商品属性">
+          <el-form-item
+            v-for="item in staticParams"
+            :key="item.attr_id"
+            :label="item.attr_name"
+            >
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
         <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
         <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
       </el-tabs>
@@ -99,7 +107,8 @@ export default {
       options: [],
       selectedOptions2: [],
       // 动态参数列表
-      dynamicParams: []
+      dynamicParams: [],
+      staticParams: []
     }
   },
   methods: {
@@ -108,14 +117,25 @@ export default {
       const response = await this.$http.get('categories?type=3');
       this.options = response.data.data;
     },
-    async loadParams() {
-      const response = await this.$http.get(`categories/${this.selectedOptions2[2]}/attributes?sel=many`);
-      this.dynamicParams = response.data.data;
+    // 获取参数
+    async loadParams(index) {
+      // 判断点击的是动态参数选项卡还是静态参数选项卡
+      const sel = index==='1'? 'many':'only';
+      const response = await this.$http.get(`categories/${this.selectedOptions2[2]}/attributes?sel=${sel}`);
+
+      if (sel==='many') {
+        // 动态参数
+        this.dynamicParams = response.data.data;
       
-      // 获取动态参数里的选项
-      this.dynamicParams.map((item)=>{
-        item.params = item.attr_vals.length===0 ? []:item.attr_vals.split(',');
-      });
+        // 获取动态参数里的选项
+        this.dynamicParams.map((item)=>{
+          item.params = item.attr_vals.length===0 ? []:item.attr_vals.split(',');
+        });     
+      } else if (sel==='only') {
+        // 静态参数
+        this.staticParams = response.data.data;
+      }
+
     },
     // 点击tab选项卡时执行的操作
     handleTabClick(tab,event) {
@@ -127,7 +147,7 @@ export default {
           return;
         }
         // 加载动态参数
-        this.loadParams();
+        this.loadParams(tab.index);
       }
     },
     // 点击级联列表时执行的函数
